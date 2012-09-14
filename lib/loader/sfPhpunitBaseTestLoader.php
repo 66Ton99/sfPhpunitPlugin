7511dd2b3704c661ae62ecf14fd2ed635f59045a
66Ton99 <Forma-PRO@66ton99.org.ua>
2012-09-14 15:30:23 +0300

    Fixed compatibility to PHPUnit 3.7

--- lib/loader/sfPhpunitBaseTestLoader.php
+++ lib/loader/sfPhpunitBaseTestLoader.php
@@ -2,7 +2,7 @@
 
 /**
  * Base phpunit's tests loader
- * Provide base functionality for all kind of loaders. 
+ * Provide base functionality for all kind of loaders.
  *
  * @package    sfPhpunitPlugin
  * @subpackage loader
@@ -11,26 +11,26 @@
 abstract class sfPhpunitBaseTestLoader
 {
   protected $_excludeDirs = array('.svn', 'fixtures');
-  
+
   /**
    * root suite
-   * 
+   *
    * @var sfBasePhpunitTestSuite
    */
   protected $_suite;
-  
+
   protected $_recursively = false;
 
   /**
    * @deprecated
-   * 
+   *
    * @return PHPUnit_Framework_TestSuite
    */
   public function getSuite()
   {
     return $this->suite();
   }
-  
+
   /**
    * @return PHPUnit_Framework_TestSuite
    */
@@ -39,14 +39,14 @@ abstract class sfPhpunitBaseTestLoader
     if (!$this->_suite) {
       $this->_initRootSuite();
     }
-    
+
     return $this->_suite;
   }
-  
+
   abstract public function load();
-  
+
   /**
-   * 
+   *
    * @return PHPUnit_Framework_TestResult
    */
   public function run($result = null)
@@ -56,28 +56,28 @@ abstract class sfPhpunitBaseTestLoader
 
   /**
    * Load unit tests
-   * 
+   *
    * @param string $path
    * @param sfBasePhpunitTestSuite $suite
    * @param bool $recursively
-   * 
+   *
    * @return void
    */
   protected function _load($path, sfBasePhpunitTestSuite $suite)
   {
     if (is_file($path)) {
-      $this->_loadFile($path, $suite);  
+      $this->_loadFile($path, $suite);
     } else {
-      $this->_loadDir($path, $suite); 
+      $this->_loadDir($path, $suite);
     }
   }
 
   /**
-   * 
+   *
    * @param string $path
    * @param sfBasePhpunitTestSuite $suite
    * @param bool $recursively
-   * 
+   *
    * @return void
    */
   protected function _loadDir($path, sfBasePhpunitTestSuite $suite)
@@ -109,10 +109,10 @@ abstract class sfPhpunitBaseTestLoader
   }
 
   /**
-   * 
+   *
    * @param string $path
    * @param sfBasePhpunitTestSuite $suite
-   * 
+   *
    * @return void
    */
   protected function _loadFile($path, sfBasePhpunitTestSuite $suite)
@@ -123,22 +123,22 @@ abstract class sfPhpunitBaseTestLoader
   }
 
   /**
-   * 
+   *
    * @param string $path
    * @throws Exception if file ending on TestSuite.class.php exists but does not contain TestSuite class.
-   * 
+   *
    * @return sfBasePhpunitTestSuite
    */
   protected function _getTestSuiteForDir($path)
   {
     if (is_file($path)) $path = dirname($path);
-    
+
     $test_suite_files = sfFinder::type('file')->name('*TestSuite.php')->maxdepth(0)->in($path);
-    
+
     if (!$path = array_shift($test_suite_files)) {
       return new sfBasePhpunitTestSuite();
     }
-    
+
     require_once $path;
     foreach ($this->_getClassessInFile($path) as $class) {
       $reflection = new ReflectionClass($class);
@@ -146,55 +146,58 @@ abstract class sfPhpunitBaseTestLoader
         return new $class();
       }
     }
-        
+
     throw new Exception('The file `'.$path.'` does not contain a suite class');
   }
-  
+
   protected function _getClassessInFile($path)
   {
     if (version_compare(PHPUnit_Runner_Version::id(), '3.4', '<')) {
       $classes = array();
       foreach (PHPUnit_Util_Class::getClassesInFile($path) as $reflection) {
-        $classes[] = $reflection->getName(); 
+        $classes[] = $reflection->getName();
       }
-      
+
       return $classes;
-    } else {
+    } elseif (version_compare(PHPUnit_Runner_Version::id(), '3.7', '<')) {
       return array_keys(PHPUnit_Util_File::getClassesInFile($path));
+    } else {
+      $stream = new PHP_Token_Stream($path);
+      return array_keys($stream->getClasses());
     }
   }
-  
+
   /**
-   * 
+   *
    * @param string $path
-   * 
+   *
    * @return string|false absolute path or false if invalid
    */
   protected function _preparePath($path)
   {
     $test_dir = $this->_getTestDir();
     $root_dir = $this->_getRootDir();
-    
+
     //absolute
-    if (false !== strpos($path, $test_dir) && file_exists($path)) {      
+    if (false !== strpos($path, $test_dir) && file_exists($path)) {
       return $path;
     }
-    
+
     //relative from the root dir
     $relativeFromRoot = realpath($root_dir.'/'.$path);
     if (file_exists($relativeFromRoot)) {
       return $relativeFromRoot;
     }
-    
-    //relative from the test dir 
+
+    //relative from the test dir
     $relativeFromTest = realpath($test_dir.'/'.$path);
     if (file_exists($relativeFromTest)) {
       return $relativeFromTest;
     }
-    
+
     return false;
   }
-  
+
   /**
    * @return string
    */
@@ -202,7 +205,7 @@ abstract class sfPhpunitBaseTestLoader
   {
     return $this->_getRootDir();
   }
-  
+
   /**
    * @return string
    */
@@ -210,9 +213,9 @@ abstract class sfPhpunitBaseTestLoader
   {
     return str_replace('\\', '/', sfConfig::get('sf_root_dir'));
   }
-  
+
   protected function _initRootSuite()
   {
     $this->_suite = $this->_getTestSuiteForDir($this->_getTestDir());
   }
-} 
+}
